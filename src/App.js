aa93b2f868e2632f76fc863fea5488e02b74050b
AlexlDP <35962219+AlexLDP@users.noreply.github.com>
2019-06-08 13:30:40 +0300

    Start

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          This Site is Under Construction.
+        </h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://Loian.dev"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+       Alex Loian Site
         </a>
       </header>
     </div>
