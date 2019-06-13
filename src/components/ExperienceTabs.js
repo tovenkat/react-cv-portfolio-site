5d103456820c88feb0e39cc0615c98776562cee4
Oleksii Loian <oleksii.loian@caspio.com>
2019-06-13 16:03:04 +0300

    Added Some items to the Drawer Area.

--- src/components/ExperienceTabs.js
+++ src/components/ExperienceTabs.js
@@ -3,7 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-
 import Details from './Details/Details';
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
