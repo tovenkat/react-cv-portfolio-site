9464a648a1697da1da7acd04cd364d920db7fee6
AlexlDP <35962219+AlexLDP@users.noreply.github.com>
2019-07-27 09:50:54 +0300

    Changed Main Page Hero

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import LazyHero from 'react-lazy-hero';
 import MacImage from "../../assets/images/alex-loian-dummy-picture-1.jpg";
-import Stock6 from "../../assets/images/alex-loian-cv-stock-photos-6.jpg";
+import Stock7 from "../../assets/images/alex-loian-cv-stock-photos-7.jpg";
 
 export default function MainPage() {
     return (
+
         <div style={{width:'100%', backgroundColor:'whitesmoke'}}>
+         
             <LazyHero opacity={0} minHeight='100vh' parallaxOffset={60} isCentered={true}  imageSrc={MacImage}>
                 <div style={{color:'black', paddingBottom:'38vh'}}>
                 <h1 >Alex Loian</h1>
@@ -18,10 +20,13 @@ export default function MainPage() {
             <div style={{height:'300px',backgroundColor:'cyan'}}>
 
             </div>
-            <LazyHero opacity={0} minHeight='100vh' parallaxOffset={60} isCentered={true}  imageSrc={Stock6}>
+            <div>
+            <LazyHero opacity={0} minHeight='400px' parallaxOffset={60} isCentered={true}  imageSrc={Stock7}>
                 <h1 style={{color:'black',textAlign:'center', paddingBottom:'200px'}}>Alex Loian</h1>
                 
             </LazyHero>
         </div>
+        </div>
+       
     )
 }
