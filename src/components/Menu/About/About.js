6f6064ebbfcae5de2becbfd6d186c5d18a9a6ce3
Oleksii Loian <oleksii.loian@caspio.com>
2019-08-05 17:28:02 +0300

    Improved About View

--- src/components/Menu/About/About.js
+++ src/components/Menu/About/About.js
@@ -1,111 +1,100 @@
-import React from 'react'
-import Avatar from '../../../assets/images/alex-loian-fron-end-developer-avatar.jpg';
+import React from "react";
+import Avatar from "../../../assets/images/alex-loian-fron-end-developer-avatar.jpg";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
-import Grid from '@material-ui/core/Grid';
+import Grid from "@material-ui/core/Grid";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Button from "@material-ui/core/Button";
 import cv from "../../../assets/images/CV-Alex-Loian-Front-End-Developer-React-Resume.pdf";
 
-const About = (props) => {
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 
-   
-  const buttonsBefore = (
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 450,
+    margin: "auto"
+  },
+  actionGroup: {
+    display: "flex",
+    justifyContent: "space-around"
+  }
+});
+const About = props => {
+  const classes = useStyles();
 
-    <Grid container  justify="space-around" spacing={3}>
-   <Grid item xs={12} sm={4}>
-     <Button fullWidth={true}
-       
-       color="primary"
-       href="https://www.linkedin.com/in/alex-loian-82828284"
-     >
-       LinkedIn
-     </Button>
-   </Grid>
-   <Grid item xs={12} sm={4}>
-     <Button fullWidth={true}
-       
-       color="primary"
-       href="https://github.com/AlexLDP"
-     >
-       GitHub
-     </Button>
-   </Grid>
-   
+  const card = (
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Alex Loian Front-End React Developer"
+          height="450"
+          image={Avatar}
+          title="Alex Loian Front-End React Developer"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Alex Loian
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Front-End React Deveper. React/JS/NodeJS
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions className={classes.actionGroup}>
+        <Button
+          size="small"
+          color="primary"
+          download="Alex-Loian-CV-React-Developer"
+          href={cv}
+        >
+          CV
+        </Button>
+        <Button size="small" color="primary" href="https://github.com/AlexLDP">
+          GitHub
+        </Button>
+        <Button
+          size="small"
+          color="primary"
+          href="https://www.linkedin.com/in/alex-loian-82828284"
+        >
+          LinkedIN
+        </Button>
+      </CardActions>
+      <CardActions className={classes.actionGroup}>
+        <Button size="small" color="primary" href="tel:0980506389">
+          Call
+        </Button>
+        <Button size="small" color="primary" href="mailto:wisehelper@gmail.com">
+          Email
+        </Button>
+        <Button size="small" color="primary" onClick={props.aboutClose}>
+          Close
+        </Button>
+      </CardActions>
+    </Card>
+  );
 
- </Grid>
+  const dialog = (
+    <Dialog
+      open={props.open}
+      TransitionComponent={props.Transition}
+      keepMounted
+      onClose={props.handleClose}
+      aria-labelledby="alert-dialog-slide-title"
+      aria-describedby="alert-dialog-slide-description"
+    >
+      {card}
+    </Dialog>
+  );
+  return dialog;
+};
 
-);
-const buttonsAfter=( <Grid container  justify="space-around" spacing={3}>
-
-<Grid item xs={12} sm={4}>
- <Button  fullWidth={true} color="primary" href="tel:0980506389">
-   Call
- </Button>
-</Grid>
-<Grid item xs={12} sm={4}>
-     <Button fullWidth={true}
-       
-       color="primary"
-       download="Alex-Loian-CV-React-Developer"
-       href={cv}
-     >
-       CV
-     </Button>
- </Grid>
-<Grid item xs={12} sm={4}>
- <Button fullWidth={true}
-   
-   color="primary"
-   href="mailto:wisehelper@gmail.com"
- >
-   Email
- </Button>
-</Grid>
-
-</Grid>
-);
-
-const dialog = (
- <Dialog
- open={props.open}
-   TransitionComponent={props.Transition}
-   keepMounted
-   onClose={props.handleClose}
-   aria-labelledby="alert-dialog-slide-title"
-   aria-describedby="alert-dialog-slide-description"
- >
-   <DialogTitle id="alert-dialog-slide-title">
-     {"Looking for React Front-End position"}
-   </DialogTitle>
-   <DialogContent>
-     {buttonsBefore}
-   
-    
-     <Grid container spacing={3}>
-       <Grid item xs={12} sm={4}>
-        <img style={{margin:'auto'}}width='100px' src={Avatar} alt="Alex Loian"/>
-       </Grid>
-       <Grid item xs={12} sm={8}>
-         <p>Location: Europe, Ukraine, Dnipro</p>
-         <p>Phone: 098-050-63-89</p>
-       </Grid>
-     </Grid>
-  
-     
-     {buttonsAfter}
-     <br/>
-    
-   </DialogContent>
-   <DialogActions>
-     <Button onClick={props.aboutClose} color="primary">
-       Close
-     </Button>
-   </DialogActions>
- </Dialog>
-);
-return dialog
-}
-
-export default About
+export default About;
