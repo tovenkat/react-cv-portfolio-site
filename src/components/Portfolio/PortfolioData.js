5dd5b93507c43a6ac26253eecb0f6a4fe76d535c
AlexlDP <35962219+AlexLDP@users.noreply.github.com>
2019-08-18 22:29:56 +0300

    Added Files to the Projects

--- src/components/Portfolio/PortfolioData.js
+++ src/components/Portfolio/PortfolioData.js
@@ -6,6 +6,7 @@ import Timer from "../../assets/images/react/alex-loian-count-down-timer.png";
 import Redux from "../../assets/images/react/alex-loian-pet-project-react-redux.png";
 import MultiStep from "../../assets/images/react/alex-loian-multi-step-form.png";
 import CvSite from "../../assets/images/react/alex-loian-cv-site.jpg";
+import Budget from "../../assets/images/react/alex-loian-budget-calculator.png";
 
 /*const Data = {
   en: {
@@ -111,6 +112,7 @@ const reactEn = [
     description: `CV Portfolio Site`,
     skills: ["React", "Material", "React Hooks"]
   },
+
   {
     title: "React REST Token",
     subTitle: "June 5,2019",
@@ -121,15 +123,6 @@ const reactEn = [
     skills: ["React", "Material", "AJAX", "REST", "Caspio"]
   },
 
-  {
-    title: "React Redux Training",
-    subTitle: "June 5,2019",
-    img: Redux,
-    demoLink: "https://alex-loian-redux-playground.web.app/",
-    gitHubLink: "https://github.com/AlexLDP/Redux-Training-Playground",
-    description: `Capsio Support Helper. REST Token Generator for Caspio REST`,
-    skills: ["React", "Material", "AJAX", "REST", "Caspio"]
-  },
   {
     title: "React Multi-Step Submission Form",
     subTitle: "July 25,2019",
@@ -158,13 +151,31 @@ const reactEn = [
     skills: ["React", "Redux", "Thunk", "Material"]
   },
   {
+    title: "React Budget Calculator",
+    subTitle: "August 19,2019",
+    img: Budget,
+    demoLink: "https://budget-calculato.web.app/",
+    gitHubLink: "https://github.com/AlexLDP/budget-calculator-react",
+    description: `Social Media Network. This app uses firebase back-end part`,
+    skills: ["React", "Redux", "Thunk", "Material"]
+  },
+  {
+    title: "React Redux Training",
+    subTitle: "June 5,2019",
+    img: Redux,
+    demoLink: "https://alex-loian-redux-playground.web.app/",
+    gitHubLink: "https://github.com/AlexLDP/Redux-Training-Playground",
+    description: `Capsio Support Helper. REST Token Generator for Caspio REST`,
+    skills: ["React", "Material", "AJAX", "REST", "Caspio"]
+  },
+  {
     title: "React Social Media Network",
     subTitle: "August 21,2019",
     img: MultiStep,
     demoLink: "https://alexldp.github.io/Multistep-Form-React/",
     gitHubLink: "https://github.com/AlexLDP/social-media-app",
-    description: `Social Media Network. This app uses firebase back-end part`,
-    skills: ["React", "NodeJS", "React Hooks", "Material", "Firebase"]
+    description: `Budget Calculator`,
+    skills: ["React", "Material", "Firebase"]
   }
 ];
 const reactRu = [
@@ -199,10 +210,19 @@ const reactRu = [
     title: "React Count Down Timer",
     subTitle: "August 20,2019",
     img: Timer,
-    demoLink: "http://alexldp.github.io/count-down-timer",
-    gitHubLink: "https://github.com/AlexLDP/count-down-timer",
+    demoLink: "https://budget-calculato.web.app/",
+    gitHubLink: "https://github.com/AlexLDP/budget-calculator-react",
     description: `Social Media Network. This app uses firebase back-end part`,
     skills: ["React", "Redux", "Thunk", "Material"]
+  },
+  {
+    title: "React Budget Calculator",
+    subTitle: "August 19,2019",
+    img: Budget,
+    demoLink: "http://alexldp.github.io/count-down-timer",
+    gitHubLink: "https://github.com/AlexLDP/count-down-timer",
+    description: `Budget Calculator`,
+    skills: ["React", "Material", "Firebase"]
   },
   {
     title: "Генератор Токенов на Реакте",
